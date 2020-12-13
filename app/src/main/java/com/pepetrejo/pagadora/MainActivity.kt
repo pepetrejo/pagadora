c7d86afbe42bf37b4708fda47f35ceeef7cfcbdd
Pepe <pepestrejo@gmail.com>
2020-12-13 20:39:49 +0100

    Se muestran los valores siempre con dos decimales y añado my nombre

--- app/src/main/java/com/pepetrejo/pagadora/MainActivity.kt
+++ app/src/main/java/com/pepetrejo/pagadora/MainActivity.kt
@@ -23,7 +23,6 @@ class MainActivity : AppCompatActivity() {
             val btnBorrar = binding.btnBorrar
             var pagadora = binding.pagadora.text.toString()
 
-            //println(String.format("%.2f",12.34654).toFloat()) redondea a 2 decimales( no me sirve)
 
             var in20 = 0
             if (binding.in20.text.isEmpty()) {
@@ -31,8 +30,8 @@ class MainActivity : AppCompatActivity() {
             } else {
                 in20 = binding.in20.text.toString().toInt()}  //pasa el EditText a Int
 
-            var total20 = in20 * 20.0 //Con ##.0 me pone un decimal, no se poner dos decimales
-            binding.calculo20.text = total20.toString() + "0€"  //he añadido el 0 porque solo me pone 1 decimal, esto es un poco cutre
+            var total20 = in20 * 20
+            binding.calculo20.text = total20.toDouble().roundDecimal(2) + "€"  // Muestra siempre 2 decimales con esta funcion
 
             var in10 = 0
             if (binding.in10.text.isEmpty()) {
@@ -40,8 +39,8 @@ class MainActivity : AppCompatActivity() {
             } else {
                 in10 = binding.in10.text.toString().toInt()}  //Muestra la var en el TextView. obliga a pasarlo a String
 
-            var total10 = in10 * 10.0
-            binding.calculo10.text = total10.toString() + "0€"
+            var total10 = in10 * 10
+            binding.calculo10.text = total10.toDouble().roundDecimal(2) + "€"
 
             var in1 = 0
             if (binding.in1.text.isEmpty()) {
@@ -49,8 +48,8 @@ class MainActivity : AppCompatActivity() {
             } else {
                 in1 = binding.in1.text.toString().toInt()}
 
-            var total1 = in1 * 1.0
-            binding.calculo1.text = total1.toString() + "0€"
+            var total1 = in1 * 1
+            binding.calculo1.text = total1.toDouble().roundDecimal(2) + "€"
 
             var in05 = 0
             if (binding.in05.text.isEmpty()) {
@@ -59,7 +58,7 @@ class MainActivity : AppCompatActivity() {
                 in05 = binding.in05.text.toString().toInt()}
 
             var total05 = in05 * 0.5
-            binding.calculo05.text = total05.toString() + "0€"
+            binding.calculo05.text = total05.toDouble().roundDecimal(2) + "€"
 
             var in01 = 0
             if (binding.in01.text.isEmpty()) {
@@ -68,9 +67,8 @@ class MainActivity : AppCompatActivity() {
                 in01 = binding.in01.text.toString().toInt()}
 
             var total01 = in01 * 0.1
-            binding.calculo01.text = total01.toString() + "0€"
-
-            //esto funciona!!!
+            binding.calculo01.text = total01.toDouble().roundDecimal(2) + "€"
+            
 
 
             /* Forma anterior que no permitía Empty (campo vacio)
@@ -81,8 +79,9 @@ class MainActivity : AppCompatActivity() {
           */
 
             var sumatotal = total20 + total10 + total1 + total05 + total01
-            binding.sumaTotal.text = sumatotal.toString() + ("0€")
+            binding.sumaTotal.text = sumatotal.toDouble().roundDecimal(2) + "€"
             println(sumatotal)
+
 
         }
 
@@ -122,5 +121,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+        // Funcion que muestra el numero de decimales que le indique con numero.toDouble().roundDecimal(aqui el numero de decimales)
+    fun Double.roundDecimal(digit: Int) = "%.${digit}f".format(this)
 
 }
